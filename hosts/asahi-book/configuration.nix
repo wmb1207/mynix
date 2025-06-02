083ca53588c170966ea543685e9c94da351e4eb3
wmb <wmb1207@proton.me>
2025-06-01 21:07:19 -0300

    m1

--- hosts/asahi-book/configuration.nix
+++ hosts/asahi-book/configuration.nix
@@ -73,6 +73,9 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
