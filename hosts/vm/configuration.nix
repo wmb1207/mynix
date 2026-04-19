720f7fc57a93e4e5391b37a1ec98a4378efa0cc8
wmb <wenceslao1207@protonmail.com>
2026-04-18 21:24:37 -0300

    update

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -78,6 +78,7 @@ in
   services.xserver.displayManager.startx.enable = lib.mkForce false;
 
   services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.defaultSession = "fvwm3";
   services.displayManager.autoLogin = {
     enable = true;
     user = "wmb";
